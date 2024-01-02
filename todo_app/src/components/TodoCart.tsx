c317f7df4bd21235e72c4963f98f350583cfe406
JahirPendhari <jahirpp1999@gmail.com>
2024-01-02 13:37:33 +0530

    Todo card Updated with Modal form

--- todo_app/src/components/TodoCart.tsx
+++ todo_app/src/components/TodoCart.tsx
@@ -1,9 +1,18 @@
-import { TODOS } from "../pages/Homepage"
+import { TODOS } from "../pages/Homepage";
 
-const  TodoCart = ({title, id,status,description}:TODOS)=>{
-    return <>
-    <h1>Single Todo</h1>
+const TodoCart = ({ id, title, description, status }: TODOS, { EditTodo }: any) => {
+  return (
+    <>
+      <div>
+        <h1>Single Todo</h1>
+        <p>ID: {id}</p>
+        <p>Title: {title}</p>
+        <p>Description: {description}</p>
+        <p>Status: {status ? "Completed" : "Incomplete"}</p>
+        <button onClick={() => EditTodo(id)}>Edit Todo</button>
+      </div>
     </>
-}
+  );
+};
 
-export {TodoCart}
+export { TodoCart };
--- todo_app/src/pages/Homepage.tsx
+++ todo_app/src/pages/Homepage.tsx
@@ -1,31 +1,87 @@
-import { useState } from "react"
+import { useState } from "react";
 import { TodoCart } from "../components/TodoCart";
 
- export type TODOS= {
-    title:String,
-    description:String,
-    status:Boolean,
-    id:Number
-}
-const Homepage=()=>{
-    const [ todos, setTodos ] = useState<TODOS[]>([{
-        title:"Learn Java",
-        description:"Learning Java First Time",
-        status:false,
-        id:1
-    }]);
+import "../App.css"
+import Modal from "../components/Modal";
+import styled from "styled-components";
 
-    return <>
-    <div>
-        <h1>Home Page</h1>
-        {
-            todos?.length >0 && todos?.map((item,i)=>{
-                return <TodoCart {...item} key ={i}/>
-            })
-        }
+export type TODOS = {
+    title: string;
+    description: string;
+    status: boolean;
+    id: number;
+    editTodo: void
+};
 
-    </div>
-    </>
-}
+const Homepage = () => {
+    const [todos, setTodos] = useState<TODOS[]>([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-export {Homepage}
+
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const editTodo = (id: number) => {
+        const updatedTodos = todos.map((todo) =>
+            todo.id === id ? { ...todo, status: !todo.status } : todo
+        );
+
+        setTodos(updatedTodos);
+    };
+
+    return (
+        <DIV>
+            <div>
+                <h1>Home Page</h1>
+                <button onClick={openModal}>Create new Todo</button>
+                <div>
+                    {todos?.length > 0 &&
+                        todos?.map((item, i) => (
+                            <TodoCart {...item} key={i} editTodo={editTodo(1)} />
+                        ))
+                    }
+                </div>
+            </div>
+            <div>
+                <Modal isOpen={isModalOpen} onClose={closeModal}>
+                    <div className="ModalBox">
+                        <h3>Create New Task</h3>
+                        <form>
+                            <input type="text"  placeholder="Enter Title"  required/>
+                            <input type="text" placeholder="Enter Description" required/>
+                            <input type="submit" value="Submit"/>
+                        </form>
+                    </div>
+                </Modal>
+            </div>
+        </DIV>
+    );
+};
+
+export { Homepage };
+
+const DIV = styled.div`
+    
+    form{
+        display: flex;
+        flex-direction: column;
+        width: 80%;
+        margin: auto;
+    }
+    input {
+        padding: 7px;
+        margin-bottom:10px;
+        border-radius:5px
+    }
+    input:nth-child(3) {
+        color:white;
+        background-color: green;
+        border: 1px solid green;
+    }
+   
+`
